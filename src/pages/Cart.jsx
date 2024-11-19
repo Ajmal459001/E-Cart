1d59e3474179ece195bdc155dcc3be19881f7a5a
Ajmal459001 <ajmallal362@gmail.com>
2024-11-19 22:11:21 +0530

    third commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,12 @@
 // rafce
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { incrementingQuantity } from '../redux/slices/cartSlice'
+import { decrementQuantity, emptyCart, incrementQuantity, removeCartItem } from '../redux/slices/cartSlice'
 
 const Cart = () => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const [cartTotal,setCartTotal] = useState(0)
   const userCart = useSelector(state=>state.cartReducer)
@@ -15,7 +16,22 @@ const Cart = () => {
       setCartTotal(userCart?.map(item=>item.totalPrice).reduce((a,b)=>a+b))
     }
   },[userCart])
+
+  const handleDecrementQuantity = (product)=>{
+    if(product?.quantity>1){
+      dispatch(decrementQuantity(product.id))
+    }else{
+      dispatch(removeCartItem(product.id))
+    }
+  }
   
+  const checkout = ()=>{
+    dispatch(emptyCart())
+    alert("Order Confirmed ...... Thank You For Purchasing With Us")
+    // redirect to home
+    navigate('/')
+  }
+
   return (
     <>
     <Header/>
@@ -46,20 +62,20 @@ const Cart = () => {
                     <td><img width={'70px'} height={'70px'} src={product?.thumbnail} alt="" /></td>
                     <td>
                       <div className="flex">
-                        <button className="font-bold">-</button>
+                        <button onClick={()=>handleDecrementQuantity(product)} className="font-bold">-</button>
                         <input style={{width:'40px'}} type="text" value={product?.quantity} className="border rounded p-1 mx-2" readOnly />
-                        <button onClick={()=>dispatch(incrementingQuantity(product?.id))} className="font-bold">+</button>
+                        <button onClick={()=>dispatch(incrementQuantity(product?.id))} className="font-bold">+</button>
                       </div>
                     </td>
                     <td>$ {product?.totalPrice}</td>
-                    <td> <button className="text-red-600"> <i className="fa-solid fa-trash"></i> </button> </td>
+                    <td> <button onClick={()=>dispatch(removeCartItem(product?.id))} className="text-red-600"> <i className="fa-solid fa-trash"></i> </button> </td>
                   </tr>
                 ))
               }
              </tbody>
            </table>
            <div className="float-right mt-5">
-             <button className="bg-red-600 rounded p-2 text-white">EMPTY CART</button>
+             <button onClick={()=>dispatch(emptyCart())} className="bg-red-600 rounded p-2 text-white">EMPTY CART</button>
              <Link to={'/'} className='bg-blue-600 rounded p-2 ms-3 text-white'>SHOP MORE</Link>
            </div>
          </div>
@@ -67,7 +83,7 @@ const Cart = () => {
            <div className="border rounded shadow p-5">
              <h2 className="text-2xl font-bold my-4">Total Amount : <span className='text-red-600'>$ {cartTotal}</span></h2>
              <hr />
-             <button className="bg-green-600 rounded p-2 text-xl text-white mt-4 w-full">CHECK OUT</button>
+             <button onClick={checkout} className="bg-green-600 rounded p-2 text-xl text-white mt-4 w-full">CHECK OUT</button>
            </div>
          </div>
        </div>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -5,7 +5,7 @@ const cartSlice = createSlice({
     initialState:[],
     reducers:{
         // action-name : reducer function
-        addToCart:(state,actionByComponent)=>{
+        addToCart : (state,actionByComponent)=>{
             const existingProduct = state.find(item=>item.id==actionByComponent.payload.id)
             if(existingProduct){
                 existingProduct.quantity++
@@ -16,7 +16,7 @@ const cartSlice = createSlice({
                 state.push({...actionByComponent.payload,quantity:1,totalPrice:actionByComponent.payload.price})
             }
         },
-        incrementingQuantity : (state,actionByCart)=>{
+        incrementQuantity : (state,actionByCart)=>{
             const existingProduct = state.find(item=>item.id==actionByCart.payload)
             if(existingProduct){
                 existingProduct.quantity++
@@ -24,9 +24,24 @@ const cartSlice = createSlice({
                 const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
                 state = [...remainingProducts,existingProduct]
             }
+        },
+        removeCartItem : (state,actionByCart)=>{
+            return state.filter(item=>item.id!=actionByCart.payload)
+        },
+        decrementQuantity : (state,actionByCart)=>{
+            const existingProduct = state.find(item=>item.id==actionByCart.payload)
+            if(existingProduct){
+                existingProduct.quantity--
+                existingProduct.totalPrice = existingProduct.quantity * existingProduct.price
+                const remainingProducts = state.filter(item=>item.id!=existingProduct.id)
+                state = [...remainingProducts,existingProduct]
+            }
+        },
+        emptyCart : (state)=>{
+            return state = []
         }
     }
 })
 
-export const {addToCart,incrementingQuantity} = cartSlice.actions
+export const {addToCart,incrementQuantity,removeCartItem,decrementQuantity,emptyCart} = cartSlice.actions
 export default cartSlice.reducer
